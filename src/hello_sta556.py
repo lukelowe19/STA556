72374d5f82bfb3356bb9584669e710d46cbf10dc
lukelowe19 <161758245+lukelowe19@users.noreply.github.com>
2026-09-01 22:08:12 +0000

    Complete Week 1 workflow activity

---
+++ src/hello_sta556.py
@@ -0,0 +1,7 @@
+import sys
+print(sys.version)
+print("Hello from STA 556!")
+name = "Lukas Lowe"
+course_number = "STA 556"
+semester = "Fall 2026"
+print(f"Welcome to {semester} {course_number}, {name}. ")
